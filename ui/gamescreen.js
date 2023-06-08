e74e687eb8062a0679e9e20b8561f0c60ba51a8c
Lavitz Star <astralvitz@pm.me>
2023-06-08 16:45:40 -0400

    welcome

--- ui/gamescreen.js
+++ ui/gamescreen.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import styles from './gamescreen.module.css';
 import { Music } from '@/components/music';
 
@@ -6,6 +7,16 @@ export function GameScreen() {
     <div className={styles.box}>
       {/* <img src="/ui_gamescreen.jpg" alt="game screen" /> */}
       <p className={styles.text}>$gp contract / 0x8F8eD076e61A27DB72a5724ea0907A66fb0ddB32</p>
+      <div className={styles.screen}>
+        <Image
+          className="screen"
+          src="/welcometothegrandexchangesmall.jpg"
+          alt="game screen"
+          width={513}
+          height={320}
+          priority
+        />
+      </div>
       {/* <p className={styles.text}>$gp balance / 00000000</p> */}
       <div className={styles.tells}>
         <Music />
