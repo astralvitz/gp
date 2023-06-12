ea25104f3ef43cdf9732c89d00cd5727440dc14b
Lavitz Star <astralvitz@pm.me>
2023-06-12 18:07:07 -0400

    use yt video above breakpoint

--- components/gamescreenplayer.js
+++ components/gamescreenplayer.js
@@ -1,25 +1,42 @@
 'use client'
 
 import React, { useRef } from 'react';
+import YouTube from 'react-youtube';
 
 export function GameScreenPlayer() {
-  const videoRef = useRef(null);
-
-  const play = () => {
-    videoRef.current.play();
+  const opts = {
+    height: '307',
+    width: '503',
+    playerVars: {
+      autoplay: 1,
+      loop: 1,
+      mute: 1,
+      playlist: 'QfM7JPFsG7M',
+      index: 0,
+    },
   };
 
-  const pause = () => {
-    videoRef.current.pause();
-  };
-
-  return (
-    <div>
-      <video ref={videoRef} src="/YGrows.mp4" muted autoPlay loop />
-      <div>
-        <button onClick={play}>Play</button>
-        <button onClick={pause}>Pause</button>
-      </div>
-    </div>
-  );
+  return <YouTube videoId="QfM7JPFsG7M" opts={opts} />;
 }
+
+// export function GameScreenPlayer() {
+//   const videoRef = useRef(null);
+
+//   const play = () => {
+//     videoRef.current.play();
+//   };
+
+//   const pause = () => {
+//     videoRef.current.pause();
+//   };
+
+//   return (
+//     <div>
+//       <video ref={videoRef} src="https://youtu.be/QfM7JPFsG7M" controls muted autoPlay loop />
+//       <div>
+//         <button onClick={play}>Play</button>
+//         <button onClick={pause}>Pause</button>
+//       </div>
+//     </div>
+//   );
+// }
--- ui/gamescreen.js
+++ ui/gamescreen.js
@@ -9,15 +9,19 @@ export function GameScreen() {
       {/* <img src="/ui_gamescreen.jpg" alt="game screen" /> */}
       <p className={styles.text}>$gp contract / 0x8F8eD076e61A27DB72a5724ea0907A66fb0ddB32</p>
       <div className={styles.screen}>
-        {/* <Image
-          className="screen"
-          src="/welcometothegrandexchangesmall.jpg"
-          alt="game screen"
-          width={513}
-          height={320}
-          priority
-        /> */}
-        <GameScreenPlayer />
+        <div className={styles.image}>
+          <Image
+            className="screen"
+            src="/welcometothegrandexchangesmall.jpg"
+            alt="game screen"
+            width={513}
+            height={320}
+            priority
+          />
+        </div>
+        <div className={styles.video}>
+          <GameScreenPlayer />
+        </div>
       </div>
       {/* <p className={styles.text}>$gp balance / 00000000</p> */}
       <div className={styles.tells}>
